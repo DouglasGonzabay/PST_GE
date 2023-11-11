2b63539a1bdda82b3a4172d7552fc6def520a161
DouglasGonzabay <dvgonzab@espol.edu.ec>
2023-11-11 12:33:57 -0500

    ingreso

---
+++ backend/src/routes/estudiantes.js
@@ -0,0 +1,20 @@
+const {Router}=require('express');
+const router = Router();
+const {getEstudiantes,getEstudiante,postEstudiante,putEstudiante,deleteEstudiante} = require ('../controllers/estudiantesControl.js');
+
+//const {getEstudiantes} = require('../controllers/estudiantesControl.js');
+
+//router.route('/').get(getEstudiantes)
+
+
+router.route('/')
+    .get(getEstudiantes)
+    .post(postEstudiante);
+
+router.route('/:id')
+    .get(getEstudiante)
+    .put(putEstudiante)
+    .delete(deleteEstudiante);
+
+module.exports = router
+
